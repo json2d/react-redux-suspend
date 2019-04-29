776d626c7b6ed56c46e9a07d07b7bf579f458025
Jason Yung <json.yung@gmail.com>
2019-04-29 16:44:20 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# react-redux-suspend
+
+😬 `React.Suspense` + `Redux` plugin
+
+## Installation
+
+```sh
+yarn add react-redux-suspend
+```
+
+## Setup
+
+Configure your store with just little bit of middleware boilerplate.
+
+```javascript
+import { createStore, applyMiddleware } from "redux";
+import { createSuspendMiddleware } from "./react-redux-suspend";
+
+const suspendMiddleware = createSuspendMiddleware();
+
+const store = createStore(reducer, applyMiddleware(suspendMiddleware));
+
+// must be called after store is created above
+suspendMiddleware.init();
+```
+
+## Usage
+
+Decorate your component with the `connect` as per usual, and then follow up with `suspend`.
+
+```javascript
+import React from "react";
+import { connect } from "react-redux";
+import { suspend } from "react-redux-suspend";
+
+let Counter = props => (
+  <p>
+    Clicked: {value} times <button onClick={onIncrement}>+</button>
+  </p>
+);
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  value: state
+});
+const mapDispatchToProps = dispatch => ({
+  onIncrement: () => dispatch({ type: "INCREMENT" })
+});
+
+Counter = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
+
+/* now for the extra part */
+
+const mapStateToInvalidation = state => state === null;
+const mapPropsToResolution = props => ({ type: "INIT_ASYNC" });
+const watchAction = action => action.type === "INIT";
+
+Counter = suspend(
+  mapStateToInvalidation, 
+  mapPropsToResolution, 
+  watchAction
+)(Counter);
+```
+
+Now while `mapStateToInvalidation` evaluates to truthy, the nearest `Suspense` component will be triggered.
+
+Of course you'll need to wrap your newly suspendable component in a `Suspense` component somewhere in your app.
+
+```javascript
+const App = props => 
+  <Provider store={store}>
+    <Suspense fallback={<div>loading...</div>}>
+      <Counter/>
+    </Suspense>
+  </Provider>
+```
+
+## License
+
+MIT
---
+++ src/react-redux-suspend.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { connect } from "react-redux";
+import { select, take, takeEvery } from "redux-saga/effects";
+import createSagaMiddleware from "redux-saga";
+
+// heres our HOC, so we can be fancy pants and decorate our React components
+export const suspend = (
+  mapStateToInvalidation, 
+  mapPropsToResolution, 
+  watchAction
+  ) => UnwrappedComponent => {
+  
+  let WrappedComponent = props => {
+    if (props.isInvalid) {
+      // fire off resolution if defined
+      if (mapPropsToResolution) {
+        props.dispatch(mapPropsToResolution(props));
+      }
+
+      throw new Promise(resolve => {
+        // fire off our suspend action to be handled by our saga
+        // note that we're also adding this Promise's the resolve fn in the payload
+        // this is the magic callback that tells the React.Suspense component to go away
+        props.dispatch(
+          actionCreators.suspend({ mapStateToInvalidation, watchAction, unsuspend: resolve })
+        );
+      });
+    }
+
+    // extract props that will live on in the
+    const { isInvalid, ...otherProps } = props;
+
+    return <UnwrappedComponent {...otherProps} />;
+  };
+
+  const mapStateToProps = state => ({
+    isInvalid: mapStateToInvalidation(state)
+  });
+
+  // yes it uses 'connect' under the hood
+  WrappedComponent = connect(mapStateToProps)(WrappedComponent);
+
+  return WrappedComponent;
+};
+
+export const actionTypes = {
+  suspend: "react-redux-suspend/SUSPEND"
+};
+
+export const actionCreators = {
+  suspend: payload => ({ type: actionTypes.suspend, payload })
+};
+
+// here's the saga creator that creates the saga that watches for validation and ends suspense
+
+function* suspendSaga(action) {
+  const { payload } = action;
+  const { watchAction, mapStateToInvalidation, unsuspend } = payload;
+
+  let isInvalid;
+  // keep checking for invalidation
+  while (true) {
+    // wait for action to get dispatched to check invalidation again
+    // if watchAction is undefined, waits for any action type
+    yield watchAction ? take(watchAction) : take();
+
+    // check invalidation again
+    isInvalid = yield select(mapStateToInvalidation);
+
+    // stop checking if valid
+    if (!isInvalid) {
+      break;
+    }
+  }
+
+  // resolve thrown Promise to end Suspense
+  unsuspend();
+}
+
+function* sagas() {
+  yield takeEvery(actionTypes.suspend, suspendSaga);
+}
+
+// wrap middleware boilerplate
+export function createSuspendMiddleware() {
+  const middleware = createSagaMiddleware()
+  middleware.init = function () { this.run(sagas) }
+  return middleware
+}
